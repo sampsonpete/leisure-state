52bcc3f5a99dd6685d97f54a9179fa39bc0d43a9
Peter Sampson <ps@assemblylondon.com>
2017-09-18 16:32:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Leisure State WordPress theme
+
+Initial theme setup using [underscores.me](http://underscores.me/) and [this gulp workflow](https://www.sitepoint.com/fast-gulp-wordpress-theme-development-workflow/).
---
+++ snippets/live-to-local-query.sql
@@ -0,0 +1,9 @@
+UPDATE wp_y4g7ynj2c5_options SET option_value = replace(option_value, 'http://leisure-state.com', 'http://localhost:8888/leisure-state') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_y4g7ynj2c5_posts SET post_content = replace(post_content, 'http://leisure-state.com', 'http://localhost:8888/leisure-state');
+UPDATE wp_y4g7ynj2c5_postmeta SET meta_value = replace(meta_value,'http://leisure-state.com','http://localhost:8888/leisure-state');
+UPDATE wp_y4g7ynj2c5_posts SET guid = REPLACE (guid, 'http://leisure-state.com', 'http://localhost:8888/leisure-state');
+
+UPDATE wp_y4g7ynj2c5_options SET option_value = replace(option_value, 'http://localhost:8888/leisure-state/build', 'http://localhost:8888/leisure-state/wordpress') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_y4g7ynj2c5_posts SET post_content = replace(post_content, 'http://localhost:8888/leisure-state/build', 'http://localhost:8888/leisure-state/wordpress');
+UPDATE wp_y4g7ynj2c5_postmeta SET meta_value = replace(meta_value,'http://localhost:8888/leisure-state/build','http://localhost:8888/leisure-state/wordpress');
+UPDATE wp_y4g7ynj2c5_posts SET guid = REPLACE (guid, 'http://localhost:8888/leisure-state/build', 'http://localhost:8888/leisure-state/wordpress');
