9e0074d5cdbdecce4ac13ba26a16cb73d0ed1e00
Peter Sampson <ps@assemblylondon.com>
2018-01-07 17:49:18 +0000

    Style archive page

--- src/template/functions.php
+++ src/template/functions.php
@@ -162,3 +162,22 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+/**
+ * Hide prefix before tag/category name on archive pages
+ */
+function my_theme_archive_title( $title ) {
+  if ( is_category() ) {
+    $title = single_cat_title( '', false );
+  } elseif ( is_tag() ) {
+    $title = single_tag_title( '', false );
+  } elseif ( is_author() ) {
+    $title = '<span class="vcard">' . get_the_author() . '</span>';
+  } elseif ( is_post_type_archive() ) {
+    $title = post_type_archive_title( '', false );
+  } elseif ( is_tax() ) {
+    $title = single_term_title( '', false );
+  }
+  return $title;
+}
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );
