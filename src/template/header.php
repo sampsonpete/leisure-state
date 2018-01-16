4a8856b37d6165dfbd78043c70e441a120e3d68c
Peter Sampson <ps@assemblylondon.com>
2018-01-16 11:45:35 +0000

    Add GA tracking code

--- src/template/header.php
+++ src/template/header.php
@@ -17,6 +17,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75391013-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-75391013-1');
+  </script>
+
   <?php wp_head(); ?>
 </head>
 
